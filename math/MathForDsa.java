a9c8f46fad999b1ce73a575bd1ea0ed5cba27f4b
Shubham <ss9971768704@gmail.com>
2024-06-25 18:14:11 +0530

    java

--- math/MathForDsa.java
+++ math/MathForDsa.java
@@ -1,9 +1,13 @@
 
+
  class MathForDsa {
 
     public static void main(String[] args) {
         //numberEvenOrOdd(6);
-       reverse(2478);
+       //reverse(2478);
+       //exponential(3, 2);
+       //countDigits(6879);
+       countDigitsByLog(0);
        
     };
     
@@ -43,4 +47,52 @@
        System.out.println(rever);
         return  rever;
     }
+
+    static double power5 (double num, double power) {
+        double res = 1;
+        for (int index = 0; index < power; index++) {
+            res = res* num;
+        }
+        System.out.println(res);
+
+        return res ;
+
+    }
+ 
+
+    static double exponential (double num, double power) {
+        double res = 1;
+        while ( power > 0) {
+            if (num % 2 != 0) {
+                res = res *num;
+                power = (power -1)/2;
+            }
+           power /=2;
+           num = num * num;
+        }
+        System.out.println(res);
+
+        return res ;
+    }
+
+    public static void countDigits (int num) {
+        int counter = 0 ;
+        while (num > 0) {
+            num /= 10;
+            counter ++;
+        }
+        System.out.println(counter);
+    }
+
+    public static void countDigitsByLog(int num){
+        int res = 0;
+        if (num == 0) {
+            System.out.println( 1);
+            return ;
+        }
+      res = (int)Math.log10(num) + 1;
+      System.out.println(res);
+    }
+
+
 }
