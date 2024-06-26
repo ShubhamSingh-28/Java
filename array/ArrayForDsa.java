d1546c77710e9574634dac038a9281f26fb84ef6
Shubham <ss9971768704@gmail.com>
2024-06-26 16:01:49 +0530

    Arrays

---
+++ array/ArrayForDsa.java
@@ -0,0 +1,30 @@
+public class ArrayForDsa {
+    public static void main(String[] args) {
+      //  int age[]; //declaration
+       // age= new int[5];  // allocation
+
+       // int age[] = new int[7];
+        
+      // int marks[]= {8,10,34,49,5,4};
+
+       //for (int index = 0; index < marks.length; index++) {
+      //   System.out.println(marks[index]);;
+       //}
+
+      // for (int i : marks) {
+       // int b = i*3;
+      //  System.out.println(b);
+      // }
+
+      String names[]={"Ram","Shyam","Gopal"};
+      /* 
+      for (String string : names) {
+        System.out.println(string);
+      }
+        */
+        
+
+
+
+    }
+}
