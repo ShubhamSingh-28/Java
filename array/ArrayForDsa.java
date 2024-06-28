52d4a4a3f86c4d445b98635fff5b6053857411bb
Shubham <ss9971768704@gmail.com>
2024-06-28 14:17:29 +0530

    2dArray

--- array/ArrayForDsa.java
+++ array/ArrayForDsa.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class ArrayForDsa {
     public static void main(String[] args) {
+      //int hed = 67;
+      //System.out.println(hed);
       
       /* int age[]; //declaration
         age= new int[5];  // allocation
@@ -13,6 +15,7 @@ public class ArrayForDsa {
        for (int index = 0; index < marks.length; index++) {
         System.out.println(marks[index]);;
        }
+        
 
        for (int i : marks) {
         int b = i*3;
@@ -24,15 +27,97 @@ public class ArrayForDsa {
       for (String string : names) {
         System.out.println(string);
       }
-         */
-
+         */ 
+        /* 
          int rollNo[] = new int[5];
          rollNo[0] = 54;
         
-         Scanner sc = new Scanner(System.in);
+         
         String b= sc.nextLine();
         System.out.println(b);
         sc.close();
+        */
+       // System.err.println("Error");
+
+      /*  int arr[]= {2,9,8,74,32};
+       print(arr);
+        deletion(arr, 3);
+        System.out.println("After Deletion");
+        print(arr);
+*/
+Scanner sc = new Scanner(System.in);
+        int m2darray[][] = new int[3][3];
+
+        for (int i = 0; i < m2darray.length; i++) {
+          for (int j = 0; j < m2darray[i].length; j++) {
+            System.out.println("Enter "+j+" element");
+            int element = sc.nextInt();
+            m2darray[i][j] = element;
+          }
+        }
+
+        print2d(m2darray);
         
     }
+
+
+    public static void print(int arr[]){
+      for (int i : arr) {
+        System.out.print(i +",");
+      }
+    }
+
+
+    public static void insert (int arr[], int pos, int element){
+        int size = arr.length;
+
+        if (pos<0 || pos> size -1) {
+          System.err.println("Wrong Position !");
+          return;
+        }
+
+        for (int i = size-2; i >=pos; i--) {
+          arr[i+1] = arr[i];
+        }
+        arr[pos]= element;
+
+    }
+
+    public static void Insertion(int arr[], int pos, int element){
+      int size = arr.length;
+      System.out.println(size);
+        if (pos<0 || pos> size -1) {
+          System.err.println("Wrong Position !");
+          return;
+        }
+
+        for (int i = size-2; i >= pos; i--) {
+          arr[i+1] =arr[i];         
+        }
+        arr[pos] = element;
+    }
+
+
+    public static void deletion(int arr[], int pos){
+      int size = arr.length;
+
+        if (pos<0 || pos> size -1) {
+          System.err.println("Wrong Position !");
+          return;
+        }
+
+        for (int i = pos; i <=size-2; i++) {
+          arr[i]=arr[i+1];
+        }
+        arr[size-1] =0;
+    }
+
+
+    public static void print2d(int arr[][]){
+     
+      for (int[] is : arr) {
+        print(is);
+      }
+    }
+
 }
